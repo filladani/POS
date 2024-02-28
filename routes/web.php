fab40ab1bb3cd8a46a9a3b619373ddd847392770
Filla Ramadhani <fillau123@gmail.com>
2024-02-28 22:54:19 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\JualController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+//Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route::prefix('category')->group(function () {
+//     Route::get('/food-beverage', [ProductsController::class, 'foodBeverage'])->name('products.food-beverage');
+//     Route::get('/beauty-health', [ProductsController::class, 'beautyHealth'])->name('products.beauty-health');
+//     Route::get('/home-care', [ProductsController::class, 'homeCare'])->name('products.home-care');
+//     Route::get('/baby-kid', [ProductsController::class, 'babyKid'])->name('products.baby-kid');
+// });
+
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
+
+Route::get('/sale', [JualController::class, 'index'])->name('jual');
+
+
